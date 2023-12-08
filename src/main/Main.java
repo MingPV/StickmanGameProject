2f53157e1587ac702eb3d018c7daf5934f32ea0a
tusaunyapat <141983396+tusaunyapat@users.noreply.github.com>
2023-12-08 15:58:19 +0700

    add music

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,21 @@
-
 package main;
 
 import javafx.application.Application;
-
 import javafx.scene.Scene;
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
 import scenes.StartScene;
 
-
+import java.io.File;
 
 public class Main extends Application {
-
     private Stage stage;
+    private MediaPlayer mediaPlayer;
+
     public static void main(String[] args) {
-
         System.out.println("Hello and welcome!");
-        Application.launch(args);
-
+        launch(args);
     }
 
     @Override
@@ -30,10 +27,18 @@ public class Main extends Application {
 
         stage.setScene(sceneStart);
         stage.show();
+        playMusic();
+
     }
 
+    public void playMusic(){
+
+        String musicFile = "res/element/itty-bitty-8-bit-kevin-macleod-main-version-03-13-7983.mp3";
+        Media backgroundMusic = new Media(new File(musicFile).toURI().toString());
+        mediaPlayer = new MediaPlayer(backgroundMusic);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
 
 
-
+    }
 }
-
--- src/scenes/StartScene.java
+++ src/scenes/StartScene.java
@@ -5,19 +5,18 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
+import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
 
-
+import java.io.File;
 
 public class StartScene extends Scene {
 
 
+
     public StartScene(Stage stage) {
         super(createStartScene(stage), 800, 600);
-
     }
 
     private static VBox createStartScene(Stage stage) {
@@ -29,24 +28,22 @@ public class StartScene extends Scene {
 
         Button btnStart = new Button("PLAY GAME");
         btnStart.setPrefSize(400, 60);
-        btnStart.setBackground(new Background(new BackgroundFill(Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
-        btnStart.setFont(new Font("Courier New", 30));
+        btnStart.setBackground(new Background(new BackgroundFill(javafx.scene.paint.Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
+        btnStart.setFont(new javafx.scene.text.Font("Courier New", 30));
         btnStart.setOnAction(event -> {
-
             stage.setScene(new GameScene(stage));
         });
 
         Button btnSetting = new Button("SETTING");
-        btnSetting.setBackground(new Background(new BackgroundFill(Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
+        btnSetting.setBackground(new Background(new BackgroundFill(javafx.scene.paint.Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
         btnSetting.setPrefSize(150, 60);
-        btnSetting.setFont(new Font("Courier New", 20));
+        btnSetting.setFont(new javafx.scene.text.Font("Courier New", 20));
 
         Button btnExit = new Button("EXIT");
-        btnExit.setBackground(new Background(new BackgroundFill(Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
+        btnExit.setBackground(new Background(new BackgroundFill(javafx.scene.paint.Color.WHITE.deriveColor(1, 1, 1, 0.8), null, null)));
         btnExit.setPrefSize(150, 60);
-        btnExit.setFont(new Font("Courier New", 20));
+        btnExit.setFont(new javafx.scene.text.Font("Courier New", 20));
         btnExit.setOnMouseClicked(event -> {
-
             stage.close();
         });
 
@@ -59,5 +56,4 @@ public class StartScene extends Scene {
 
         return home;
     }
-
 }
